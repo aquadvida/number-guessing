800fa88c15576a33c6eca9b0f92561622c84ab90
David Klausen <david.klausen@icloud.com>
2024-09-05 16:23:51 +0200

    optimized code

---
+++ game.py
@@ -0,0 +1,23 @@
+import random
+
+x = int(input("Enter lower boundary "))
+y = int(input("Enter higher boundary "))
+z = ""
+
+count = 0
+number = random.randint(x, y)
+
+while z != number:
+    if z == "":
+         z = int(input("Guess the number "))
+         count = count + 1
+    if z > number:
+        print("Lower!")
+        z = int(input("Guess again "))
+        count = count + 1
+    elif z < number:
+        print("Higher!")
+        z = int(input("Guess again "))
+        count = count + 1
+
+print(number, "is correct! It took you", count, "tries!")
