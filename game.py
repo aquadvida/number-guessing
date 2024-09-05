6851b85b8bb045d58267e3184cb918b4e06249c6
David Klausen <david.klausen@icloud.com>
2024-09-05 16:27:53 +0200

    fixed indentation

--- game.py
+++ game.py
@@ -3,15 +3,14 @@ import random
 x = int(input("Enter lower boundary "))
 y = int(input("Enter higher boundary "))
 z = ""
-
 count = 0
 number = random.randint(x, y)
 
 while z != number:
     if z == "":
-         z = int(input("Guess the number "))
-         count = count + 1
-    if z > number:
+        z = int(input("Guess the number "))
+        count = count + 1
+    elif z > number:
         print("Lower!")
         z = int(input("Guess again "))
         count = count + 1
